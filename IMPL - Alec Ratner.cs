27861879f57e4b4ee3c571145147d67b5891dcc8
Alec Ratner <89555635+AI3C@users.noreply.github.com>
2022-09-26 14:53:34 -0400

    Add files via upload

---
+++ IMPL - Alec Ratner.cs
@@ -0,0 +1,42 @@
+﻿using NHibernate.Mapping.ByCode;
+using System;
+Import java.util.ArrayList;
+import java.util.Arrays;
+
+namespace ClassExample.Controllers
+{
+    public class Class
+    {
+        /*Take in a string(from a user or a file), compute the total number of
+        occurrences of each character in the ASCII set. Print out the top three characters
+        (with the most occurrences). Print out how many characters in the ASCII set you didn’t detect.
+        Then print out the string in reverse order.
+ 
+        1. Take a string from the user
+        2. Turn every character of string into an arraylist
+        3. Handle the arraylist to display 3 statements
+
+        Display 3 print statements:
+            - Top 3 occuring strings
+            - How many ASCII characters were not in the string
+            - Print the string in reverse order*/
+
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please input a string: \n");
+
+            string input = Console.ReadLine();
+
+            // split string by no space
+            String[] inputSplit = str.split("");
+
+
+        }
+
+        //Def of done
+
+
+
+    }
+}
